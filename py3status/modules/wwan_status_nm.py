91bf6d9933aaae1de4cd5b516e3d19e053353ed7
Cyril Levis <levis.cyril@gmail.com>
2018-03-14 22:41:25 +0100

    -add ip support -rollback to use my data dict -remove consider_3G_degraded for the moment -some cleanup

--- py3status/modules/wwan_status_nm.py
+++ py3status/modules/wwan_status_nm.py
@@ -6,18 +6,10 @@ based on ModemManager, NetworkManager and dbus.
 Configuration parameters:
     cache_timeout: How often we refresh this module in seconds.
         (default 5)
-    consider_3G_degraded: If set to True, only 4G-networks will be
-        considered 'good'; 3G connections are shown
-        as 'degraded', which is yellow by default. Mostly
-        useful if you want to keep track of where there
-        is a 4G connection.
-        (default False)
     format_down: What to display when the modem is not plugged in.
         (default 'WWAN: {state} - {operator} {netgen} ({signal}%)')
-    format_error: What to display when modem can't be accessed.
-        (default 'WWAN: {error}')
     format_up: What to display upon regular connection
-        (default 'WWAN: {state} - {operator} {netgen} ({signal}%)')
+        (default 'WWAN: {state} - {operator} {netgen} ({signal}%) - {ipv4_address} {ipv6_address}')
     modem: The modem device to use. If None
         will use first find modem or
         use 'busctl introspect org.freedesktop.ModemManager1 \
@@ -27,7 +19,6 @@ Configuration parameters:
 
 Color options:
     color_bad: Error or no connection
-    color_degraded: Low generation connection eg 2G
     color_good: Good connection
 
 Requires:
@@ -38,7 +29,7 @@ Requires:
 @author Cyril Levis <levis.cyril@gmail.com>
 
 SAMPLE OUTPUT
-{'color': '#00FF00', 'full_text': u'WWAN: Connected - Bouygues Telecom 4G (19%)'}
+{'color': '#00FF00', 'full_text': u'WWAN: Connected - Bouygues Telecom 4G (19%) - 10.10.0.94 - ::1'}
 
 off
 {'color': '#FF0000', 'full_text': u'WWAN: Disconnected - Bouygues Telecom 4G (12%)'}
@@ -48,6 +39,8 @@ from pydbus import SystemBus
 
 STRING_WRONG_MODEM = "wrong or any modem"
 STRING_UNKNOW_OPERATOR = "unknow operator"
+DBUS_MODEM_PATH = 'Modem/'
+DBUS_BEARER_PATH = 'Bearer/'
 
 
 class Py3status:
@@ -55,20 +48,15 @@ class Py3status:
     """
     # available configuration parameters
     cache_timeout = 5
-    consider_3G_degraded = False
-    format_down = 'WWAN: {state} - {operator} {netgen} ({signal}%)'
-    format_error = 'WWAN: {error}'
-    format_up = 'WWAN: {state} - {operator} {netgen} ({signal}%)'
+    format_down = 'WWAN: {status} - {operator} {netgen} ({signal}%)'
+    format_up = 'WWAN: {status} - {operator} {netgen} ({signal}%) - ip:{ip_address} ip4:{ipv4_address}, ip6:{ipv6_address}'
     modem = None
 
-    def wwan_status_nm(self):
-        response = {}
-        response['cached_until'] = self.py3.time_in(self.cache_timeout)
-
-        states = {10: "Connecting", 11: "Connected"}
+    def post_config_hook(self):
+        self.states = {10: "Connecting", 11: "Connected"}
 
         # https://www.freedesktop.org/software/ModemManager/api/1.0.0/ModemManager-Flags-and-Enumerations.html#MMModemAccessTechnology
-        speed = {
+        self.speed = {
             16384: 'LTE',
             8192: 'EVDOB',
             4096: 'EVDOA',
@@ -86,11 +74,15 @@ class Py3status:
             0: 'POTS'
         }
 
+    def wwan_status_nm(self):
+        response = {}
+        response['cached_until'] = self.py3.time_in(self.cache_timeout)
+
         bus = SystemBus()
         for id in range(0, 20):
 
             # find the modem
-            device = 'Modem/' + str(id)
+            device = str(DBUS_MODEM_PATH) + str(id)
 
             try:
                 proxy = bus.get('.ModemManager1', device)
@@ -105,9 +97,9 @@ class Py3status:
 
                     # start to build return data dict
                     data = {
-                        'state': 'Disconnected',
+                        'status': 'Disconnected',
                         'signal': status['signal-quality'][0],
-                        'netgen': speed[status['access-technologies']]
+                        'netgen': self.speed[status['access-technologies']]
                     }
 
                     # if registred on network, get operator name
@@ -123,33 +115,65 @@ class Py3status:
                         break
 
                     # if connected or connecting
-                    if status['state'] == 11 or status['state'] == 10:
-                        data['state'] = states[status['state']]
-                        response['full_text'] = self.py3.safe_format(
-                            self.format_up, data)
-                        response['color'] = self.py3.COLOR_GOOD
+                    if status['state'] == 10 or status['state'] == 11:
+                        # Get status in human readable
+                        data['status'] = self.states[status['state']]
+
+                        # Get ipv4 network config
+                        try:
+                            bearer = proxy.Bearers[0]
+                            bearer_proxy = bus.get('.ModemManager1', bearer)
+                            ipv4 = bearer_proxy.Ip4Config
+                            data['ipv4_address'] = ipv4['address']
+                            data['ipv4_dns1'] = ipv4['dns1']
+                            data['ipv4_dns2'] = ipv4['dns2']
+
+                        except:
+                            data['ipv6_address'] = ''
+                            data['ipv6_dns1'] = ''
+                            data['ipv6_dns2'] = ''
+
+                        # Get ipv6 network config
+                        try:
+                            bearer = proxy.Bearers[0]
+                            bearer_proxy = bus.get('.ModemManager1', bearer)
+                            ipv6 = bearer_proxy.Ip6Config
+                            data['ipv6_address'] = ipv6['address']
+                            data['ipv6_dns1'] = ipv6['dns1']
+                            data['ipv6_dns2'] = ipv6['dns2']
+
+                        except:
+                            data['ipv6_address'] = ''
+                            data['ipv6_dns1'] = ''
+                            data['ipv6_dns2'] = ''
+
+                        if data['ipv6_address'] != '':
+                            color = self.py3.COLOR_GOOD
+                            data['ip_address'] = data['ipv6_address']
+                        elif data['ipv4_address'] != '':
+                            color = self.py3.COLOR_GOOD
+                            data['ip_address'] = data['ipv4_address']
+                        else:
+                            color = self.py3.COLOR_BAD
+                            data['ip_address'] = 'no network config'
+
+                        full_text = self.py3.safe_format(self.format_up, data)
+
                     # else disconnected
                     else:
-                        response['full_text'] = self.py3.safe_format(
+                        full_text = self.py3.safe_format(
                             self.format_down, data)
-                        response['color'] = self.py3.COLOR_BAD
-                    return response
+                        color = self.py3.COLOR_BAD
 
-                else:
-                    response['full_text'] = self.py3.safe_format(
-                        self.format_error, dict(error=''))
-                    response['color'] = self.py3.COLOR_BAD
-
-                    self.py3.error(STRING_WRONG_MODEM)
-                    return response
+                    return {'full_text': full_text, 'color': color}
 
             except:
                 pass
 
         # if there is no modem
-        response['full_text'] = ''
-        response['color'] = self.py3.COLOR_BAD
-        return response
+        full_text = ''
+        color = self.py3.COLOR_BAD
+        return {'full_text': full_text, 'color': color}
 
 
 if __name__ == "__main__":
